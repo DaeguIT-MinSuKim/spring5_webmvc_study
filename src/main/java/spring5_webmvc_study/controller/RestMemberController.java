666542917f908c20bb4acfc56bcdf0eb0ed68468
mskim <net94@naver.com>
2021-05-27 12:43:55 +0900

    aa

--- src/main/java/spring5_webmvc_study/controller/RestMemberController.java
+++ src/main/java/spring5_webmvc_study/controller/RestMemberController.java
@@ -4,16 +4,24 @@ import java.io.IOException;
 import java.util.List;
 
 import javax.servlet.http.HttpServletResponse;
+import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class RestMemberController {
 	@Autowired
     private MemberDao memberDao;
+	
+	@Autowired
+    private MemberRegisterService registerService;
+
 
 	@GetMapping("/api/members")
     public List<Member> members(){
@@ -28,6 +36,22 @@ public class RestMemberController {
             return null;
         }
         return member;
+    }
+
+    @PostMapping("/api/members")
+    public void newMember(@RequestBody RegisterRequest regReq, Errors errors,  HttpServletResponse response) throws IOException {
+    	try {
+            new RegisterRequestValidator().validate(regReq, errors);
+            if (errors.hasErrors()) {
+                response.sendError(HttpServletResponse.SC_BAD_REQUEST);
+                return;
+            }
+            Long newMemberId = registerService.regist(regReq);
+            response.setHeader("Location", "/api/members/" + newMemberId);
+            response.setStatus(HttpServletResponse.SC_CREATED);
+        }catch (DuplicateMemberException e) {
+            response.sendError(HttpServletResponse.SC_CONFLICT);
+        }
     }
 
 }
